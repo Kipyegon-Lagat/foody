bde2ebc5298f5090d11397ea387101d3233788d2
Kipyegon Lagat <kipyegongithub@gmail.com>
2025-07-24 17:41:02 +0300

    Update Contact.jsx

--- Food-Court-main/src/components/Contact.jsx
+++ Food-Court-main/src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     <div className="flex justify-center items-center flex-col m-10 p-10">
         <h1 className=" font-bold mb-5 ">Contact Us</h1>
         <p className="m-3 ">For any queries or inquiries, please reach out to our customer support team.</p>
-        <p className="m-3">Email:  support@example.com</p>
+        <p className="m-3">Email:  support@foody.com</p>
         <p className="m-3">Phone: 123-456-7890</p>
         <button className="border border-black">submit</button>
   
